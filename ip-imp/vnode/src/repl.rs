f560594d08d6d780ecb091e4b8afbe410a86407c
spoonmilk <haku31229@gmail.com>
2024-10-17 22:56:17 -0400

    repl back

--- ip-imp/vnode/src/repl.rs
+++ ip-imp/vnode/src/repl.rs
@@ -1,79 +1,77 @@
-// use easy_repl::{command, CommandStatus, Repl};
-// use easy_repl::anyhow::{self, Context};
-// use std::env;
-// use library::ip_data_types::{NodeType, CmdType};
-// use tokio::sync::mpsc::Sender;
-// 
-// #[tokio::main]
-// pub async fn run_repl(_n_type: NodeType, send_nchan: Sender<CmdType>) -> anyhow::Result<()> {
-//     #[rustfmt::skip]
-//     let mut repl = Repl::builder()
-//         .add("li", command! {
-//             "li: List interfaces",
-//             () => || {
-//                 let ls_cmd: CmdType = CmdType::Li;
-//                 send_cmd(ls_cmd, send_nchan);
-//                 Ok(CommandStatus::Done)
-//             }
-//         })
-//         .add("ln", command! {
-//             "ln: List neighbors",
-//             () => || {
-//                 let ln_cmd: CmdType = CmdType::Ln;
-//                 send_cmd(ln_cmd, send_nchan);
-//                 Ok(CommandStatus::Done)
-//             }
-//         })
-//         .add("lr", command! {
-//             "lr: List routes",
-//             () => || {
-//                 let lr_cmd: CmdType = CmdType::Lr;
-//                 send_cmd(lr_cmd, send_nchan);
-//                 Ok(CommandStatus::Done)
-//             }
-//         })
-//         .add("down", command! {
-//             "down: disable interface <ifname>",
-//             (ifname: String) => |ifname| {
-//                 let down_cmd: CmdType = CmdType::Down(ifname);
-//                 send_cmd(down_cmd, send_nchan);
-//                 Ok(CommandStatus::Done)
-//             }
-//         })
-//         .add("up", command! {
-//             "up: enable interface <ifname>",
-//             (ifname: String) => |ifname| {
-//                 let up_cmd: CmdType = CmdType::Up(ifname);
-//                 Ok(CommandStatus::Done)
-//             }
-//         })
-//         .add("send", command! {
-//             "send: Send a test packet",
-//             (addr: String, message: String) => |addr: String, message: String| {
-//                 let i: usize = 2;
-//                 let args: Vec<String> = env::args().collect();
-//                 let retstr: String = String::from("");
-//                 while i < args.len() {
-//                     retstr.push_str(&args[i]);
-//                     if args[i].contains("\n") {
-//                         break;
-//                     }
-//                 }
-//                 let sender_cmd: CmdType = CmdType::Send(addr, retstr);
-//                 send_cmd(sender_cmd, send_nchan);
-//                 // Send packet with message
-//                 Ok(CommandStatus::Done)
-//             }
-//         })
-//         .build()
-//         .context("Failed to create repl")?;
-// 
-//     repl.run().context("Critical REPL error")
-// }
-// 
-// async fn send_cmd(command: CmdType, send_nchan: Sender<CmdType>) {
-//     match send_nchan.send(command).await {
-//         Err(e) => eprintln!("Error: Encountered error while sending command to node: {}", e),
-//         _ => (),
-//     }
-// }
+use easy_repl::{command, CommandStatus, Repl};
+use easy_repl::anyhow::{self, Context};
+use std::env;
+use library::ip_data_types::{NodeType, CmdType};
+use tokio::sync::mpsc::Sender;
+
+pub async fn run_repl(_n_type: NodeType, send_nchan: Sender<CmdType>) -> anyhow::Result<()> {
+    let mut repl = Repl::builder()
+        .add("li", command! {
+            "li: List interfaces",
+            () => || {
+                let ls_cmd: CmdType = CmdType::Li;
+                send_cmd(ls_cmd, send_nchan);
+                Ok(CommandStatus::Done)
+            }
+        })
+        .add("ln", command! {
+            "ln: List neighbors",
+            () => || {
+                let ln_cmd: CmdType = CmdType::Ln;
+                send_cmd(ln_cmd, send_nchan);
+                Ok(CommandStatus::Done)
+            }
+        })
+        .add("lr", command! {
+            "lr: List routes",
+            () => || {
+                let lr_cmd: CmdType = CmdType::Lr;
+                send_cmd(lr_cmd, send_nchan);
+                Ok(CommandStatus::Done)
+            }
+        })
+        .add("down", command! {
+            "down: disable interface <ifname>",
+            (ifname: String) => |ifname| {
+                let down_cmd: CmdType = CmdType::Down(ifname);
+                send_cmd(down_cmd, send_nchan);
+                Ok(CommandStatus::Done)
+            }
+        })
+        .add("up", command! {
+            "up: enable interface <ifname>",
+            (ifname: String) => |ifname| {
+                let up_cmd: CmdType = CmdType::Up(ifname);
+                Ok(CommandStatus::Done)
+            }
+        })
+        .add("send", command! {
+            "send: Send a test packet",
+            (addr: String, message: String) => |addr: String, message: String| {
+                let i: usize = 2;
+                let args: Vec<String> = env::args().collect();
+                let retstr: String = String::from("");
+                while i < args.len() {
+                    retstr.push_str(&args[i]);
+                    if args[i].contains("\n") {
+                        break;
+                    }
+                }
+                let sender_cmd: CmdType = CmdType::Send(addr, retstr);
+                send_cmd(sender_cmd, send_nchan);
+                // Send packet with message
+                Ok(CommandStatus::Done)
+            }
+        })
+        .build()
+        .context("Failed to create repl")?;
+
+    repl.run().context("Critical REPL error")
+}
+
+async fn send_cmd(command: CmdType, send_nchan: Sender<CmdType>) {
+    match send_nchan.send(command).await {
+        Err(e) => eprintln!("Error: Encountered error while sending command to node: {}", e),
+        _ => (),
+    }
+}
